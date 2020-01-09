b2813a4c406fa9d35330d7205d56d3fcedd80603
Henry Boyd <AmorphousCube@gmail.com>
2020-01-09 21:12:19 +0000

    implemented core functionality of index file

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  //Sum all the numbers in the array
+  //loop through array
+  //if we find an array we recur
+  //if we find a value we add
+  //return sum
+let sum = 0;
+
+  array.forEach(element => {
+    if(Array.isArray(element)) {
+      sum += sumItems(element);
+    } else {
+      sum += element;
+    }
+  });
+  return sum;
 }
 
 module.exports = sumItems;
